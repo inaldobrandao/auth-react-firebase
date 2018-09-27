d7923a41583f429e6c6b85f021dc484c8356208f
InaldoBrandao <inaldopj@gmail.com>
2018-09-27 00:39:35 -0300

    create container auth and configurate router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Dashboard from './containers/dashboard/Dashboard';
+import Auth from './containers/auth/Auth';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,7 +10,9 @@ class App extends Component {
     return (
       <div className="App">
         <Switch>
+          <Route path="/" component={Dashboard}/>
           <Route path="/dashboard" component={Dashboard}/>
+          <Route path="/auth" component={Auth}/>
         </Switch>
       </div>
     );
---
+++ src/containers/auth/Auth.js
@@ -0,0 +1,15 @@
+import React,{ Component } from 'react';
+
+class Auth extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                Auth
+            </div>
+        )
+    }
+}
